f040c39edeb9310c0dbe26160ef6d02517dc3c60
Gabriel <gabriel688466@gmail.com>
2018-05-29 01:24:10 -0600

    First Commit

---
+++ Tip Calculator/tip-calculator.py
@@ -0,0 +1,10 @@
+print("""Hello!
+Welcome to Gabe\'s tip Calculator'""")
+
+cost_of_food_input = float(input("Enter Cost of Food: "))
+tip_input = float(input("Amount to tip: "))
+total = cost_of_food_input + cost_of_food_input * tip_input
+
+print(total)
+
+print(Hope you have a Good One!)
