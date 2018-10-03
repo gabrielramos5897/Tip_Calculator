cb32021da497cd5523d379b44129c172e8cfc350
unknown <gabriel688466@gmail.com>
2018-10-03 02:40:55 -0600

    Added Texas tax rate

--- tip-calculator.py
+++ tip-calculator.py
@@ -3,13 +3,19 @@ print("""Hello!
 Welcome to Gabe\'s tip Calculator""")
 
 cost_of_food = float(input("Enter Cost of Food: "))
-tip_input = float(input("Amount to tip: "))
+sales_tax = cost_of_food * 0.0825
+food_w_tax = cost_of_food + sales_tax
+tip_input = float(input("Tip Percentage: "))
 tip_conversion = tip_input / 100
 tip = cost_of_food * tip_conversion
-total = cost_of_food + tip
-print("Your time comes out to ", tip)
+tip_new = round(tip, 2)
+sales_tax = cost_of_food * 0.0825
+total = food_w_tax + tip
+print("Your Tip comes out to $", tip_new)
 new_total = round(total, 2)
-print("Your total is $ ", new_total)
+new_sales_tax = round(sales_tax, 2)
+print("Sales Tax:",new_sales_tax)
+print("Your total is $", new_total)
 
 
 #Income tax Calculator
