ee89dcf0a8e112684063ac8119165e9603b1c39f
unknown <gabriel688466@gmail.com>
2018-10-03 02:15:51 -0600

    Updated file

--- tip-calculator.py
+++ tip-calculator.py
@@ -1,10 +1,15 @@
+#This is a tip calculator
 print("""Hello!
-Welcome to Gabe\'s tip Calculator'""")
+Welcome to Gabe\'s tip Calculator""")
 
-cost_of_food_input = float(input("Enter Cost of Food: "))
+cost_of_food = float(input("Enter Cost of Food: "))
 tip_input = float(input("Amount to tip: "))
-total = cost_of_food_input + cost_of_food_input * tip_input
+tip_conversion = tip_input / 100
+tip = cost_of_food * tip_conversion
+total = cost_of_food + tip
+print("Your time comes out to ", tip)
+new_total = round(total, 2)
+print("Your total is $ ", new_total)
 
-print(total)
 
-print("Hope you have a Good One!")
+#Income tax Calculator
